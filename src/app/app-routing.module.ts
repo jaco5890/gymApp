9fbe2655ccfe6e71bfa926632b8e318aa0ad3215
emile.boshoff <emile.boshoff@symbiotics.co.za>
2020-05-26 21:31:00 +0200

    updated routing to have all lazy loaded routes on main routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: './menu/menu.module#MenuPageModule'
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
-  { path: 'ammenities',
-   loadChildren: './ammenities/ammenities.module#AmmenitiesPageModule' },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'ammenities',
+    loadChildren: () => import('./ammenities/ammenities.module').then(m => m.AmmenitiesPageModule)
+  },
   {
     path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+    loadChildren: () => import('./contact/contact.module').then(m => m.ContactPageModule)
+  },
+  {
+    path: 'gallery',
+    loadChildren: () => import('./gallery/gallery.module').then(m => m.GalleryPageModule)
   }
 
 ];
